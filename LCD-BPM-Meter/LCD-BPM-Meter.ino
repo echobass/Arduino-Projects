3901d3ce4c0f4cadaba106d49c530aab81895589
Willem Brosz <willem.brosz@gmail.com>
2018-05-21 21:49:40 -0700

    First version of BPM meter

--- LCD-BPM-Meter/LCD-BPM-Meter.ino
+++ LCD-BPM-Meter/LCD-BPM-Meter.ino
@@ -1,17 +1,7 @@
-/*********************
-
-Example code for the Adafruit RGB Character LCD Shield and Library
-
-This code displays text on the shield, and also reads the buttons on the keypad.
-When a button is pressed, the backlight changes color.
-
-**********************/
-
 // include the library code:
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
-
 
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
@@ -19,7 +9,6 @@ When a button is pressed, the backlight changes color.
 // the I2C bus.
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
-// These #defines make it easy to set the backlight color
 #define RED 0x1
 #define YELLOW 0x3
 #define GREEN 0x2
@@ -28,53 +17,130 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 #define VIOLET 0x5
 #define WHITE 0x7
 
-void setup() {
-  // Debugging output
-  Serial.begin(9600);
-  // set up the LCD's number of columns and rows: 
-  lcd.begin(16, 2);
+#define CHARACTERS 16
+#define ROWS 2
 
-  // Print a message to the LCD. We track how long it takes since
-  // this library has been optimized a bit and we're proud of it :)
-  int time = millis();
-  lcd.print("Hello, world!");
-  time = millis() - time;
-  Serial.print("Took "); Serial.print(time); Serial.println(" ms");
-  lcd.setBacklight(WHITE);
+/*
+ * TODO
+ * - Two modes: Manual & Automatic
+ * - Flashing beat indicator
+ * - Backlight flash beat indicator
+ * - Spectrum display if audio present
+ * - Only update bpm on button press
+ * - Indicator for reset/hold
+ */
+
+// Min BPM: 50 (1.2s) ?
+// Max BPM: 190 (0.31s) ?
+
+#define RESET_SECONDS 2
+
+unsigned long lastPress = 0;
+uint8_t lastButtons = 0;
+
+float bpm = 0.0;
+float trailingAverageBpm = 0.0; // Unimplemented
+
+void clearRow(int row) {
+  // Better: Pad the update with spaces
+  if (row != 0 && row != 1)
+    return;
+  lcd.setCursor(0, row);
+  lcd.print("     "); // 5 Spaces
 }
 
-uint8_t i=0;
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis()/1000);
+void drawUpdate() {
+  clearRow(0);
+  lcd.setCursor(0, 0);
+  if (bpm == 0) {
+    lcd.print("000.0");
+  } else {
+    lcd.print(bpm, 1);
+  }
+}
 
+void updateBpm() {
+  unsigned long now = millis();
+
+  if (lastPress == 0) {
+    lastPress = now;
+    bpm = 0.0;
+    return;
+  }
+  
+  unsigned long elapsed = now - lastPress;
+ 
+  if (elapsed >= (RESET_SECONDS * 1000)) {
+    lastPress = now;
+    bpm = 0.0;
+    return;
+  }
+  
+  bpm = 60.0 / (elapsed / 1000.0);
+  // TODO: Update trailing average
+  lastPress = now;
+}
+
+void loop() {
   uint8_t buttons = lcd.readButtons();
 
   if (buttons) {
-    lcd.clear();
-    lcd.setCursor(0,0);
-    if (buttons & BUTTON_UP) {
-      lcd.print("UP ");
-      lcd.setBacklight(RED);
+    if (buttons & BUTTON_UP) { // Up Button
+      if (lastButtons & BUTTON_UP) { // Hold
+      } else { // New Press
+      }
     }
-    if (buttons & BUTTON_DOWN) {
-      lcd.print("DOWN ");
-      lcd.setBacklight(YELLOW);
+    if (buttons & BUTTON_DOWN) { // Down Button
+      if (lastButtons & BUTTON_DOWN) { // Hold
+      } else { // New Press
+      }
     }
-    if (buttons & BUTTON_LEFT) {
-      lcd.print("LEFT ");
-      lcd.setBacklight(GREEN);
+    if (buttons & BUTTON_LEFT) { // Left Button
+      if (lastButtons & BUTTON_LEFT) { // Hold
+      } else { // New Press
+        updateBpm();
+        drawUpdate();
+        lastPress = millis();
+      }
     }
-    if (buttons & BUTTON_RIGHT) {
-      lcd.print("RIGHT ");
-      lcd.setBacklight(TEAL);
+    if (buttons & BUTTON_RIGHT) { // Right Button
+      if (lastButtons & BUTTON_RIGHT) { // Hold
+      } else { // New Press
+      }
     }
-    if (buttons & BUTTON_SELECT) {
-      lcd.print("SELECT ");
-      lcd.setBacklight(VIOLET);
+    if (buttons & BUTTON_SELECT) { // Select Button
+      if (lastButtons & BUTTON_SELECT) { // Hold
+      } else { // New Press
+        updateBpm();
+        drawUpdate();
+        lastPress = millis();
+      }
     }
   }
+
+  lastButtons = buttons;
+}
+
+void draw() {
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print("             BPM");
+  drawUpdate();
+}
+
+void setup() {
+  Serial.begin(9600);
+  
+  lcd.begin(CHARACTERS, ROWS);
+  
+  lcd.setBacklight(WHITE);
+  lcd.setCursor(0, 0);
+  lcd.print(" Hakuna");
+  lcd.setCursor(0, 1);
+  lcd.print("        Matata!");
+  delay(2000);
+  
+  lcd.clear();
+  lcd.setBacklight(VIOLET);
+  draw();
 }
